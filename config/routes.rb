556bafcd00f600583aa20c4bda4779182a705193
Gabe Kessler <gabe@impdesigns.com>
2009-10-15 00:29:20 -0400

    added typography and reset css

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "pages", :action => "redirect"
+  map.home '', :controller => "pages", :action => "redirect"
 
   # See how all your routes lay out with "rake routes"
 
