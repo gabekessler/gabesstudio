e4ca5c8e2967f02f625c3aa57c5fdd783e8fbcfe
Gabe Kessler <gabe@impdesigns.com>
2009-11-30 23:15:43 -0500

    added restful authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  map.resources :users
+
+  map.resource :session
+
   map.resources :projects, :has_many => :gallery_images, :shallow => true
 
   map.resources :gallery_images, :collection => { :all_images => :get } 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091119015919) do
+ActiveRecord::Schema.define(:version => 20091127192435) do
 
   create_table "gallery_images", :force => true do |t|
     t.string   "title",              :limit => 100
@@ -57,5 +57,23 @@ ActiveRecord::Schema.define(:version => 20091119015919) do
   create_table "tags", :force => true do |t|
     t.string "name"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+    t.string   "activation_code",           :limit => 40
+    t.datetime "activated_at"
+    t.string   "state",                                    :default => "passive"
+    t.datetime "deleted_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
