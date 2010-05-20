90df0138b8c4dfe01e332ef60b7773bda7f42979
Gabe Kessler <gabe@impdesigns.com>
2010-05-20 13:41:46 -0400

    more trying for iphone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,5 +38,10 @@ module ApplicationHelper
   	   @gallery_image = GalleryImage.all.rand.image(:large)
   	 end
   	 
+  	 
+  	 # Request from an iPhone or iPod touch? (Mobile Safari user agent)
+     def iphone_user_agent?
+       request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+     end
   
 end
