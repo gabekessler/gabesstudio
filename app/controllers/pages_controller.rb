95e43fa8687d198a61d904438d6e96d859ad4c40
Gabe Kessler <gabe@impdesigns.com>
2010-05-20 14:15:09 -0400

    meh

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-layout :determine_layout
+before_filter :determine_layout
 before_filter :find_tags  
 before_filter :tag
 before_filter :login_required, :except => [:show, :index, :redirect]
