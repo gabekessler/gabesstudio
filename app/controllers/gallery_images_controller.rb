644a582bb9e098474c6b292d50bfee9bd7cc039b
Gabe Kessler <gabe@impdesigns.com>
2010-05-21 14:40:12 -0400

    image show

--- app/controllers/gallery_images_controller.rb
+++ app/controllers/gallery_images_controller.rb
@@ -28,6 +28,7 @@ class GalleryImagesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @gallery_image }
+      format.iphone {render :layout => 'application'}
     end
   end
 
