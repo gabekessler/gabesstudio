c96df5197a06c6f6079172ec73563090f655ce39
Gabe Kessler <gabe@impdesigns.com>
2010-05-28 09:25:36 -0400

    trying

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.home ''respond_to do |format|
+  map.home respond_to do |format|
     format.html {:controller => "pages", :action => "redirect" }
     format.iphone {:controller => "pages", :action => "index" }
   end
