27c918d80ae076183a6040cae7250db1b6871dd6
Gabe Kessler <gabe@impdesigns.com>
2010-05-28 09:08:28 -0400

    added false to layout var

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,7 +25,7 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @project }
-      format.iphone {render :layout => 'application'}
+      format.iphone {render :layout => false}
     end
   end
 
