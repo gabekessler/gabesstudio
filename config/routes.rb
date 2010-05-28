15d2bd802b976bd6748111146c623ee3c4e4dba9
Gabe Kessler <gabe@impdesigns.com>
2010-05-28 09:33:31 -0400

    trying

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,11 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.home respond_to do |format|
-    format.html {:controller => "pages", :action => "redirect" }
-    format.iphone {:controller => "projects", :action => "index" }
+  map.home ''
+    respond_to do |format|
+      format.html {:controller => "pages", :action => "redirect" }
+      format.iphone {:controller => "projects", :action => "index" }
+    end
   end
 
   # See how all your routes lay out with "rake routes"
