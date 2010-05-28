25c60f25433065e75bc2064210fb3a743bd83b47
Gabe Kessler <gabe@impdesigns.com>
2010-05-28 13:05:00 -0400

    more testing

--- public/javascripts/iphone.js
+++ public/javascripts/iphone.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 function loadPage(url) {
    if (url == undefined) {
-      $('#container').load('/projects/index.iphone.erb #content', hijackLinks);
+      $('#container').load('../projects/index.iphone.erb #content', hijackLinks);
    } else {
       $('#container').load(url + ' #content', hijackLinks);
    }
