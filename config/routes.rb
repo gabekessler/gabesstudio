dcdcc90c68b5fe9ce0354ab576d4e2f98f065731
Gabe Kessler <gabe@impdesigns.com>
2010-05-28 09:28:16 -0400

    switching home route to projects index

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.home respond_to do |format|
     format.html {:controller => "pages", :action => "redirect" }
-    format.iphone {:controller => "pages", :action => "index" }
+    format.iphone {:controller => "projects", :action => "index" }
   end
 
   # See how all your routes lay out with "rake routes"
