dc3d538a4654d0944f3310f814babdea26229336
Gabe Kessler <gabe@impdesigns.com>
2010-05-27 22:59:02 -0400

    adding javascript nav

---
+++ public/javascripts/iphone.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){ 
+    loadPage();
+});
+function loadPage(url) {
+   if (url == undefined) {
+      $('#container').load('pages/index.iphone.erb #header ul', hijackLinks);
+   } else {
+      $('#container').load(url + ' #content', hijackLinks);
+   }
+}
+function hijackLinks() {
+    $('#container a').click(function(e){
+        e.preventDefault();
+        loadPage(e.target.href);
+    });
+}
